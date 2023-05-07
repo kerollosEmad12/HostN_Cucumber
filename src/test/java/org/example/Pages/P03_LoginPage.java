7024f53f914398f0dfbbe69d05d1a79d3efc3be8
KerolloEmad12 <kyrillosemad06@gmail.com>
2023-05-07 11:47:56 +0300

    HostN_Project

--- src/test/java/org/example/Pages/P03_LoginPage.java
+++ src/test/java/org/example/Pages/P03_LoginPage.java
@@ -1,4 +1,31 @@
 package org.example.Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import static org.example.StepDefs.Hooks.driver;
 public class P03_LoginPage {
+
+    public P03_LoginPage ()
+    {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy (css = "a[href=\"https://dev-api.host-n.com/login\"]")
+    public WebElement loginTab;
+
+    @FindBy (css = "input[id=\"phone\"]")
+    public WebElement phone;
+
+    @FindBy (css = "input[type=\"password\"]")
+    public WebElement pass;
+
+    @FindBy (css = "button[type=\"submit\"]")
+    public WebElement submit;
+
+    @FindBy (css = "div[class=\"alert alert-danger\"]")
+    public WebElement actualMge;
+
+    @FindBy (css = "div[class=\"alert alert-danger\"]")
+    public WebElement color;
 }
--- src/test/java/org/example/StepDefs/Login.java
+++ src/test/java/org/example/StepDefs/Login.java
@@ -2,6 +2,8 @@ package org.example.StepDefs;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.example.Pages.P01_HomePage;
+import org.example.Pages.P03_LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -13,6 +15,7 @@ import static org.example.StepDefs.Hooks.driver;
 public class Login {
 
     JavascriptExecutor js =  (JavascriptExecutor) driver;
+    P03_LoginPage login = new P03_LoginPage();
     @Given("User Navigate To home page")
     public void Step1 ()
     {
@@ -22,27 +25,24 @@ public class Login {
     @Then("login is successfully")
     public void Step2()
     {
-        WebElement Btn = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
-        js.executeScript("arguments[0].click();", Btn);
+        js.executeScript("arguments[0].click();", login.submit);
     }
 
     @And("user click on login Btn")
     public void userClickOnLoginBtn() throws InterruptedException {
-        driver.findElement(By.cssSelector("a[href=\"https://dev-api.host-n.com/login\"]")).click();
+       login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user keep phone number is an empty")
     public void Step3() throws InterruptedException {
-        WebElement num = driver.findElement(By.cssSelector("input[id=\"phone\"]"));
-        js.executeScript("arguments[0].value='';", num);
+        js.executeScript("arguments[0].value='';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user enter valid password")
     public void Step4() throws InterruptedException {
-        WebElement pass = driver.findElement(By.cssSelector("input[type=\"password\"]"));
-        js.executeScript("arguments[0].value='123456789';", pass);
+        js.executeScript("arguments[0].value='123456789';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
@@ -55,29 +55,26 @@ public class Login {
 
     @And("User click on login Btn")
     public void Step6() throws InterruptedException {
-        driver.findElement(By.cssSelector("a[href=\"https://dev-api.host-n.com/login\"]")).click();
+        login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user enter invalid phone number format")
     public void Step7() throws InterruptedException {
-        WebElement num = driver.findElement(By.cssSelector("input[id=\"phone\"]"));
-        js.executeScript("arguments[0].value='010335847';", num);
+        js.executeScript("arguments[0].value='010335847';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User enter valid password")
     public void Step8() throws InterruptedException {
-        WebElement pass = driver.findElement(By.cssSelector("input[type=\"password\"]"));
-        js.executeScript("arguments[0].value='123456789';", pass);
+        js.executeScript("arguments[0].value='123456789';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("Login is successfully")
     public void Step9()
     {
-        WebElement Btn = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
-        js.executeScript("arguments[0].click();", Btn);
+        js.executeScript("arguments[0].click();", login.submit);
 
 
         SoftAssert soft = new SoftAssert();
@@ -88,12 +85,12 @@ public class Login {
             );
 
         //2- message content or equal "Login credential are invalid."
-        String actualMsg = driver.findElement(By.cssSelector("div[class=\"alert alert-danger\"]")).getText();
+        String actualMsg = login.actualMge.getText();
         soft.assertTrue(actualMsg.contains("Login credential are invalid."),
                 "actualMsg : "+actualMsg + " | " + "expected Msg : "+"Login credential are invalid."
         );
         //3- message color is green using RGBA or Hex
-        String actualColorRGBA = driver.findElement(By.cssSelector("div[class=\"alert alert-danger\"]")).getCssValue("border-color");
+        String actualColorRGBA = login.color.getCssValue("border-color");
         String actualColorHex = Color.fromString(actualColorRGBA).asHex();
         soft.assertEquals(actualColorHex, "#f5c6cb");
 
@@ -109,29 +106,26 @@ public class Login {
 
     @And("User[B] click on login Btn")
     public void Step11() throws InterruptedException {
-        driver.findElement(By.cssSelector("a[href=\"https://dev-api.host-n.com/login\"]")).click();
+        login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user[C] enter phone number not registered")
     public void Step12() throws InterruptedException {
-        WebElement num = driver.findElement(By.cssSelector("input[id=\"phone\"]"));
-        js.executeScript("arguments[0].value='01036954123';", num);
+        js.executeScript("arguments[0].value='01036954123';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User[D] enter valid password")
     public void Step13() throws InterruptedException {
-        WebElement pass = driver.findElement(By.cssSelector("input[type=\"password\"]"));
-        js.executeScript("arguments[0].value='123456789';", pass);
+        js.executeScript("arguments[0].value='123456789';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("Login[E] is successfully")
     public void Step14()
     {
-        WebElement Btn = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
-        js.executeScript("arguments[0].click();", Btn);
+        js.executeScript("arguments[0].click();", login.submit);
 
         SoftAssert soft = new SoftAssert();
         //1- assert url equal or contains https://dev-api.host-n.com/login
@@ -141,12 +135,12 @@ public class Login {
         );
 
         //2- message content or equal "Login credential are invalid."
-        String actualMsg = driver.findElement(By.cssSelector("div[class=\"alert alert-danger\"]")).getText();
+        String actualMsg = login.actualMge.getText();
         soft.assertTrue(actualMsg.contains("Login credential are invalid."),
                 "actualMsg : "+actualMsg + " | " + "expected Msg : "+"Login credential are invalid."
         );
         //3- message color is green using RGBA or Hex
-        String actualColorRGBA = driver.findElement(By.cssSelector("div[class=\"alert alert-danger\"]")).getCssValue("border-color");
+        String actualColorRGBA = login.color.getCssValue("border-color");
         String actualColorHex = Color.fromString(actualColorRGBA).asHex();
         soft.assertEquals(actualColorHex, "#f5c6cb");
 
@@ -162,29 +156,26 @@ public class Login {
 
     @And("User[B] Click on login Btn")
     public void Step16() throws InterruptedException {
-        driver.findElement(By.cssSelector("a[href=\"https://dev-api.host-n.com/login\"]")).click();
+        login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user[C] enter valid phone number")
     public void Step17() throws InterruptedException {
-        WebElement num = driver.findElement(By.cssSelector("input[id=\"phone\"]"));
-        js.executeScript("arguments[0].value='01033584776';", num);
+        js.executeScript("arguments[0].value='01033584776';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User[D] enter password is an empty")
     public void Step18() throws InterruptedException {
-        WebElement pass = driver.findElement(By.cssSelector("input[type=\"password\"]"));
-        js.executeScript("arguments[0].value='';", pass);
+        js.executeScript("arguments[0].value='';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("login[E] is Successfully")
     public void Step19()
     {
-        WebElement Btn = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
-        js.executeScript("arguments[0].click();", Btn);
+        js.executeScript("arguments[0].click();", login.submit);
     }
 
     @Given("USer[A] Navigate To Home Page")
@@ -195,29 +186,26 @@ public class Login {
 
     @And("USer[B] Click on login Btn")
     public void Step21() throws InterruptedException {
-        driver.findElement(By.cssSelector("a[href=\"https://dev-api.host-n.com/login\"]")).click();
+        login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User[C] enter valid phone number")
     public void Step22() throws InterruptedException {
-        WebElement num = driver.findElement(By.cssSelector("input[id=\"phone\"]"));
-        js.executeScript("arguments[0].value='01033584776';", num);
+        js.executeScript("arguments[0].value='01033584776';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User[D] enter password without one num")
     public void Step23() throws InterruptedException {
-        WebElement pass = driver.findElement(By.cssSelector("input[type=\"password\"]"));
-        js.executeScript("arguments[0].value='1234567';", pass);
+        js.executeScript("arguments[0].value='1234567';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("LOgin[E] is Successfully")
     public void Step24()
     {
-        WebElement Btn = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
-        js.executeScript("arguments[0].click();", Btn);
+        js.executeScript("arguments[0].click();", login.submit);
 
         SoftAssert soft = new SoftAssert();
         //1- assert url equal or contains https://dev-api.host-n.com/login
@@ -227,12 +215,12 @@ public class Login {
         );
 
         //2- message content or equal "Login credential are invalid."
-        String actualMsg = driver.findElement(By.cssSelector("div[class=\"alert alert-danger\"]")).getText();
+        String actualMsg = login.actualMge.getText();
         soft.assertTrue(actualMsg.contains("Login credential are invalid."),
                 "actualMsg : "+actualMsg + " | " + "expected Msg : "+"Login credential are invalid."
         );
         //3- message color is green using RGBA or Hex
-        String actualColorRGBA = driver.findElement(By.cssSelector("div[class=\"alert alert-danger\"]")).getCssValue("border-color");
+        String actualColorRGBA = login.color.getCssValue("border-color");
         String actualColorHex = Color.fromString(actualColorRGBA).asHex();
         soft.assertEquals(actualColorHex, "#f5c6cb");
 
@@ -248,29 +236,26 @@ public class Login {
 
     @And("USER[B] Click on login Btn")
     public void Step26() throws InterruptedException {
-        driver.findElement(By.cssSelector("a[href=\"https://dev-api.host-n.com/login\"]")).click();
+        login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("USER[C] enter valid phone number")
     public void Step27() throws InterruptedException {
-        WebElement num = driver.findElement(By.cssSelector("input[id=\"phone\"]"));
-        js.executeScript("arguments[0].value='01033584776';", num);
+        js.executeScript("arguments[0].value='01033584776';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("USER[D] enter valid password")
     public void Step28() throws InterruptedException {
-        WebElement pass = driver.findElement(By.cssSelector("input[type=\"password\"]"));
-        js.executeScript("arguments[0].value='12345678';", pass);
+        js.executeScript("arguments[0].value='12345678';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("LOGIN[E] Is Successfully")
     public void Step29()
     {
-        WebElement Btn = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
-        js.executeScript("arguments[0].click();", Btn);
+        js.executeScript("arguments[0].click();", login.submit);
 
         SoftAssert soft = new SoftAssert();
         //1- assert url equal or contains https://dev-api.host-n.com/login
