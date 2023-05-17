2edc5513250ef755b142a7cff1a3633fc9d514ec
KerolloEmad12 <kyrillosemad06@gmail.com>
2023-05-17 11:54:12 +0300

    HostN_Project

--- src/test/java/org/example/StepDefs/Login.java
+++ src/test/java/org/example/StepDefs/Login.java
@@ -26,50 +26,50 @@ public class Login {
     }
 
     @And("user click on login Btn")
-    public void userClickOnLoginBtn() throws InterruptedException {
+    public void Step3() throws InterruptedException {
        login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user keep phone number is an empty")
-    public void Step3() throws InterruptedException {
+    public void Step4() throws InterruptedException {
         js.executeScript("arguments[0].value='';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user enter valid password")
-    public void Step4() throws InterruptedException {
+    public void Step5() throws InterruptedException {
         js.executeScript("arguments[0].value='123456789';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
 
     @Given("User Navigate To Home page")
-    public void Step5()
+    public void Step6()
     {
         driver.navigate().to("https://dev-api.host-n.com/");
     }
 
     @And("User click on login Btn")
-    public void Step6() throws InterruptedException {
+    public void Step7() throws InterruptedException {
         login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user enter invalid phone number format")
-    public void Step7() throws InterruptedException {
+    public void Step8() throws InterruptedException {
         js.executeScript("arguments[0].value='010335847';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User enter valid password")
-    public void Step8() throws InterruptedException {
+    public void Step9() throws InterruptedException {
         js.executeScript("arguments[0].value='123456789';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("Login is successfully")
-    public void Step9()
+    public void Step10()
     {
         js.executeScript("arguments[0].click();", login.submit);
 
@@ -96,31 +96,31 @@ public class Login {
     }
 
     @Given("User[A] Navigate To Home page")
-    public void Step10()
+    public void Step11()
     {
         driver.navigate().to("https://dev-api.host-n.com/");
     }
 
     @And("User[B] click on login Btn")
-    public void Step11() throws InterruptedException {
+    public void Step12() throws InterruptedException {
         login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user[C] enter phone number not registered")
-    public void Step12() throws InterruptedException {
+    public void Step13() throws InterruptedException {
         js.executeScript("arguments[0].value='01036954123';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User[D] enter valid password")
-    public void Step13() throws InterruptedException {
+    public void Step14() throws InterruptedException {
         js.executeScript("arguments[0].value='123456789';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("Login[E] is successfully")
-    public void Step14()
+    public void Step15()
     {
         js.executeScript("arguments[0].click();", login.submit);
 
@@ -146,61 +146,61 @@ public class Login {
     }
 
     @Given("User[A] Navigate To Home Page")
-    public void Step15()
+    public void Step16()
     {
         driver.navigate().to("https://dev-api.host-n.com/");
     }
 
     @And("User[B] Click on login Btn")
-    public void Step16() throws InterruptedException {
+    public void Step17() throws InterruptedException {
         login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("user[C] enter valid phone number")
-    public void Step17() throws InterruptedException {
+    public void Step18() throws InterruptedException {
         js.executeScript("arguments[0].value='01033584776';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User[D] enter password is an empty")
-    public void Step18() throws InterruptedException {
+    public void Step19() throws InterruptedException {
         js.executeScript("arguments[0].value='';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("login[E] is Successfully")
-    public void Step19()
+    public void Step20()
     {
         js.executeScript("arguments[0].click();", login.submit);
     }
 
     @Given("USer[A] Navigate To Home Page")
-    public void Step20()
+    public void Step21()
     {
         driver.navigate().to("https://dev-api.host-n.com/");
     }
 
     @And("USer[B] Click on login Btn")
-    public void Step21() throws InterruptedException {
+    public void Step22() throws InterruptedException {
         login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User[C] enter valid phone number")
-    public void Step22() throws InterruptedException {
+    public void Step23() throws InterruptedException {
         js.executeScript("arguments[0].value='01033584776';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("User[D] enter password without one num")
-    public void Step23() throws InterruptedException {
+    public void Step24() throws InterruptedException {
         js.executeScript("arguments[0].value='1234567';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("LOgin[E] is Successfully")
-    public void Step24()
+    public void Step25()
     {
         js.executeScript("arguments[0].click();", login.submit);
 
@@ -226,31 +226,31 @@ public class Login {
     }
 
     @Given("USER[A] Navigate To Home Page")
-    public void Step25()
+    public void Step26()
     {
         driver.navigate().to("https://dev-api.host-n.com/");
     }
 
     @And("USER[B] Click on login Btn")
-    public void Step26() throws InterruptedException {
+    public void Step27() throws InterruptedException {
         login.loginTab.click();
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("USER[C] enter valid phone number")
-    public void Step27() throws InterruptedException {
+    public void Step28() throws InterruptedException {
         js.executeScript("arguments[0].value='01033584776';", login.phone);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @And("USER[D] enter valid password")
-    public void Step28() throws InterruptedException {
+    public void Step29() throws InterruptedException {
         js.executeScript("arguments[0].value='12345678';", login.pass);
         Thread.sleep(Duration.ofSeconds(3));
     }
 
     @Then("LOGIN[E] Is Successfully")
-    public void Step29()
+    public void Step30()
     {
         js.executeScript("arguments[0].click();", login.submit);
 
